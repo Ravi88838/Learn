3f760a711270c28d50ca64f50bc122819d3b68d6
CameoItDevOps <itdevelopment@cameoindia.com>
2023-04-26 22:06:29 +0530

    sa

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import { Fragment, useEffect, useState } from "react";
 
 function App() {
-  const [state, setState] = useState("");
+  const [state, setState] = useState("Hi");
   useEffect(() => {});
   return (
     <Fragment>
       <h1>Hello 2</h1>
+      {state}
     </Fragment>
   );
 }
